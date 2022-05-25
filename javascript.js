389a3b1bb78f9ece0ad228925258f952f89e832e
N-H-Arif <nokimulhasanarif256999@gmail.com>
2022-05-25 11:05:05 +0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,36 @@
+
+// in- line comment
+
+/*multiline comment */
+
+var myName = "Arif"
+let myName1 = "Hasan"
+const Lastname = "Arif"
+
+var a;
+var b= 9;
+a=b
+console.log(a)
+
+// js is case sensitive
+
+//js cannot read nested string for "".have to put \ . but '' and `` is fine for nested.
+
+var test = " lalalalalal a \"lalalalalalaa\" lalalalalala"
+console.log(test)
+
+var test1 = ' lalalalalal a "lalalalalalaa" lalalalalala'
+console.log(test1)
+
+//strings are immutable
+
+//array
+
+var h = ["fafaef",44]
+
+//nested array
+
+var g = [["efewfef",34],["aafeaf",45]]
+
+
+
